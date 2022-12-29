be19b23a747d955ca6721a283d0f489d01ff8872
jafet <jafethyahuma@gmail.com>
2022-12-29 11:26:20 +0300

    added automatic path routing

--- script.py
+++ script.py
@@ -6,19 +6,20 @@ import json
 
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path to credentials json file")
+cred = credentials.Certificate(os.getcwd()+'\cred\cred.json')
 
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+print( 'checking for json files in : '+os.getcwd()+'\jsonFiles')
 
-for filename in os.listdir('path where json files to be uploaded is located'):
+for filename in os.listdir(os.getcwd()+'\jsonFiles'):
 
     if filename.endswith('.json'):
 
         collectionName = filename.split('.')[0] # filename minus ext will be used as collection name
-
-        f = open( filename, 'r')
+        
+        f = open( os.getcwd()+'\jsonFiles\\' +filename, 'r')
 
         docs = json.loads(f.read())
 
